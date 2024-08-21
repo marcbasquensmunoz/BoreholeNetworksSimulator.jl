290be6bb3f2d530757ea63d5c0e221bf4e3c49f3
Marc Basquens <marc5@kth.se>
2024-08-21 15:23:04 +0200

    Test

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,18 @@
 using Documenter, Literate, BoreholeNetworksSimulator
 
 pages = [
-    "Introduction" => "index.md",
-    "Tutorial" => [
+    "Introduction" => "index.md"
+    #="Tutorial" => [
         "Basic tutorial" => "tutorial.md",
         "Non-history method" => "nonhistory.md",
         "Running from python" => "python.md"
     ],
-    "API" => "api.md"
+    "API" => "api.md"=#
 ]
 
 dir = @__DIR__
-Literate.markdown("$dir/src/tutorial.jl", "$dir/src")
-Literate.markdown("$dir/src/nonhistory.jl", "$dir/src")
+#Literate.markdown("$dir/src/tutorial.jl", "$dir/src")
+#Literate.markdown("$dir/src/nonhistory.jl", "$dir/src")
 makedocs(
     pages=pages,
     sitename="BoreholeNetworksSimulator.jl",
