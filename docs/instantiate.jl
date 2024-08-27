e172ff8be5ad0ed14c80ed8a21e6d809f6da6891
Marc Basquens <marc5@kth.se>
2024-08-27 09:00:52 +0200

    Test

--- docs/instantiate.jl
+++ docs/instantiate.jl
@@ -5,6 +5,9 @@ geothermal_registry = "https://github.com/marcbasquensmunoz/geothermal_registry"
 documenter_directory = joinpath(@__DIR__, "..")
 project_directory = isempty(ARGS) ? @__DIR__() : joinpath(pwd(), ARGS[1])
 
+@show documenter_directory
+@show project_directory
+
 cd(project_directory) do
     Pkg.Registry.add(RegistrySpec(url = geothermal_registry))
     Pkg.Registry.add("General")
