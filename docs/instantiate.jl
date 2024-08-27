bc4d777e22fb63fbae9deaacd001682ae1fa3762
Marc Basquens <marc5@kth.se>
2024-08-27 08:58:01 +0200

    Test CI

---
+++ docs/instantiate.jl
@@ -0,0 +1,14 @@
+using Pkg
+
+geothermal_registry = "https://github.com/marcbasquensmunoz/geothermal_registry"
+
+documenter_directory = joinpath(@__DIR__, "..")
+project_directory = isempty(ARGS) ? @__DIR__() : joinpath(pwd(), ARGS[1])
+
+cd(project_directory) do
+    Pkg.Registry.add(RegistrySpec(url = geothermal_registry))
+    Pkg.Registry.add("General")
+    Pkg.activate(project_directory)
+    Pkg.develop(PackageSpec(path = documenter_directory))
+    Pkg.instantiate()
+end
