5029f197d81f6e0cb4cfc4e208c12aedaab3977a
Marc Basquens <marc5@kth.se>
2024-09-27 11:34:09 +0200

    Test

--- docs/make.jl
+++ docs/make.jl
@@ -17,7 +17,7 @@ pages = [
 ]
 
 dir = @__DIR__
-#Literate.markdown("$dir/src/tutorial.jl", "$dir/src")
+Literate.markdown("$dir/src/tutorial.jl", "$dir/src")
 Literate.markdown("$dir/src/nonhistory.jl", "$dir/src")
 makedocs(
     pages = pages,
