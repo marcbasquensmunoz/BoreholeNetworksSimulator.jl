229ed1fd0ac2f85dfff73ff7b86998d2224b99f0
Marc Basquens <marc5@kth.se>
2024-10-04 11:36:13 +0200

    Test

---
+++ test/examples/runexamples.jl
@@ -0,0 +1,15 @@
+using Pkg
+
+geothermal_registry = "https://github.com/marcbasquensmunoz/geothermal_registry"
+
+project_directory = joinpath(@__DIR__, "..")
+plots_directory = joinpath(project_directory, "BNSPlots")
+
+cd(project_directory) do
+    Pkg.Registry.add(RegistrySpec(url = geothermal_registry))
+    Pkg.Registry.add("General")
+    Pkg.activate(; temp=true)
+    Pkg.develop(PackageSpec(path = plots_directory))
+    Pkg.develop(PackageSpec(path = project_directory))
+    Pkg.instantiate()
+end
