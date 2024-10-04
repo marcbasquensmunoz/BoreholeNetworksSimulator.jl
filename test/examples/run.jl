a07ad032b038ebe2da37ea752365644a9550a933
Marc Basquens <marc5@kth.se>
2024-10-04 14:04:56 +0200

    Test

---
+++ test/examples/run.jl
@@ -0,0 +1,6 @@
+
+include("../../examples/Braedstrup/main.jl")
+include("../../examples/g-function/main.jl")
+include("../../examples/tekniska/constant_m.jl")
+include("../../examples/tekniska/prop_m.jl")
+include("../../examples/tekniska/toggle.jl")
