4a47a9e3b0cf255f60dfaa84d4d720d78462d96a
Marc Basquens <marc5@kth.se>
2024-11-04 09:10:29 +0100

    Remove unnecessary dependencies

--- src/modular/responses/nonhistory/q_coef.jl
+++ src/modular/responses/nonhistory/q_coef.jl
@@ -1,16 +1,16 @@
 
 function q_coef(::NoBoundary, medium, method, setup, i)
-    constant_integral(medium, method, setup, i) + constant_coef(method, i)
+    constant_integral(medium, setup, i) + constant_coef(method, i)
 end
 
 function q_coef(::DirichletBoundaryCondition, medium, method, setup, i)
     @unpack expΔt, w, ζ = method
-    constant_integral(medium, method, setup, i) - constant_integral(medium, method, image(setup), i) + constant_coef(method, i)
+    constant_integral(medium, setup, i) - constant_integral(medium, image(setup), i) + constant_coef(method, i)
 end
 
 function q_coef(::NeumannBoundaryCondition, medium, method, setup, i)
     @unpack expΔt, w, ζ = method
-    constant_integral(medium, method, setup, i) + constant_integral(medium, method, image(setup), i) + constant_coef(method, i)
+    constant_integral(medium, setup, i) + constant_integral(medium, image(setup), i) + constant_coef(method, i)
 end
 
 function constant_coef(method::NonHistoryMethod, i)
@@ -19,7 +19,7 @@ function constant_coef(method::NonHistoryMethod, i)
     @views -dot(w[:, i], aux)
 end
 
-function constant_integral(medium::GroundMedium, method, setup::SegmentToSegment, i)
+function constant_integral(medium::GroundMedium, setup::SegmentToSegment, i)
     @unpack D1, H1, D2, H2, σ = setup
     @unpack λ = medium
 
@@ -27,9 +27,8 @@ function constant_integral(medium::GroundMedium, method, setup::SegmentToSegment
     1/(4π*λ*H2) * (β(D1+H1-D2-H2) + β(D1-D2) - β(D1+H1-D2) - β(D1-D2-H2))
 end
 
-function constant_integral(medium::GroundMedium, method, setup::SegmentToPoint, i)
+function constant_integral(medium::GroundMedium, setup::SegmentToPoint, i)
     @unpack D, H, z, σ = setup
-    @unpack expΔt, w, ζ = method
     @unpack λ = medium
 
     1/(4π*λ) * log((z-D+sqrt(σ^2+(z-D)^2)) / (z-D-H+sqrt(σ^2+(z-D-H)^2)))
