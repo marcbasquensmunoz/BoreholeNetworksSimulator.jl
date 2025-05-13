6883f9377be5e9b344393dd4bc8f311d15e3020d
Marc Basquens <marc5@kth.se>
2025-05-13 10:10:33 +0200

    Remove tekniska from example set

--- test/examples/run.jl
+++ test/examples/run.jl
@@ -4,12 +4,14 @@ examples_dir = "../../examples"
 include("$examples_dir/Braedstrup/main.jl")
 @info "Running g-function"
 include("$examples_dir/g-function/main.jl")
+#=
 @info "Running Tekniska: constant m"
 include("$examples_dir/tekniska/constant_m.jl")
 @info "Running Tekniska: proportional m"
 include("$examples_dir/tekniska/prop_m.jl")
 @info "Running Tekniska: toggle"
 include("$examples_dir/tekniska/toggle.jl")
+=#
 @info "Running partialization"
 include("$examples_dir/partialization/interval.jl")
 include("$examples_dir/partialization/temp_diff.jl")
