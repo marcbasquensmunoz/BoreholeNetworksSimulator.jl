87eec7cc2c167298309ad23d9b38364c6678c8b8
Marc Basquens <marc5@kth.se>
2025-05-13 09:22:47 +0200

    Fix github test packages

--- test/examples/initialize.jl
+++ test/examples/initialize.jl
@@ -14,4 +14,4 @@ cd(project_directory) do
     Pkg.instantiate()
 end
 
-Pkg.add(["CSV", "Colors", "Parameters", "WGLMakie", "CairoMakie"])
+Pkg.add(["CSV", "Colors", "Parameters", "WGLMakie", "CairoMakie", "PyhtonCall", "CondaPkg", "Statistics"])
